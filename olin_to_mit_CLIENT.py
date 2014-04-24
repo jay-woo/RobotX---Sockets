8e4a52fed2575012937365f241d8dc6924f18fb5
Jay Woo <Jay.Woo@students.olin.edu>
2014-04-24 18:07:40 -0400

    Sends XML data from the server to the client

--- olin_to_mit_CLIENT.py
+++ olin_to_mit_CLIENT.py
@@ -1,26 +1,27 @@
 import socket
 
+"""Number of bits to be transferred"""
+fileSize = 1024
+
+"""Sets up sockets for each client to receive data"""
 clientSocketA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocketB = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSocketC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 host = socket.gethostname()
-portA = 1513
-portB = 1616
-portC = 1781
+portA = 3232
+portB = 4343
+portC = 5454
 
 clientSocketA.connect((host, portA))
 clientSocketB.connect((host, portB))
 clientSocketC.connect((host, portC))
 
-tmA = clientSocketA.recv(1024)
-tmB = clientSocketB.recv(1024)
-tmC = clientSocketC.recv(1024)
+"""Each socket receives data from the server"""
+dataA = clientSocketA.recv(fileSize)
+dataB = clientSocketB.recv(fileSize)
+dataC = clientSocketC.recv(fileSize)
 
-clientSocketA.close()
-clientSocketB.close()
-clientSocketC.close()
-
-print("The time got from the server is %s" % tmA.decode('ascii'))
-print("The time got from the server is %s" % tmB.decode('ascii'))
-print("The time got from the server is %s" % tmC.decode('ascii'))
+print("Data A: %s" % dataA.decode('ascii'))
+print("Data B: %s" % dataB.decode('ascii'))
+print("Data C: %s" % dataC.decode('ascii'))
--- olin_to_mit_SERVER.py
+++ olin_to_mit_SERVER.py
@@ -1,37 +1,47 @@
 import socket
-import time
+import sys
 
+"""Creates 3 server sockets"""
 serverSocketA = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocketB = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverSocketC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+"""Selects an IP address/port and binds each socket"""
 host = socket.gethostname() #For testing purposes
-portA = 1513
-portB = 1616
-portC = 1781
+portA = 3232
+portB = 4343
+portC = 5454
 
 serverSocketA.bind((host, portA))
 serverSocketB.bind((host, portB))
 serverSocketC.bind((host, portC))
 
+"""Waits until all connections are made and accepts them"""
 serverSocketA.listen(5)
 serverSocketB.listen(5)
 serverSocketC.listen(5)
 
+clientSocketA, addrA = serverSocketA.accept()
+clientSocketB, addrB = serverSocketB.accept()
+clientSocketC, addrC = serverSocketC.accept()
+
+"""Delivers data to each client
+        A - Nav data
+        B - LIDAR stream
+        C - Image data         """
 while True:
-    clientSocketA, addrA = serverSocketA.accept()
-    clientSocketB, addrB = serverSocketB.accept()
-    clientSocketC, addrC = serverSocketC.accept()
+    fileA = open('sample1.xml', 'rb').read()
+    fileB = open('sample2.xml', 'rb').read()
+    fileC = open('sample3.xml', 'rb').read()
     
-    print("Got a connection from %s" % str(addrA))
-    print("Got a connection from %s" % str(addrB))
-    print("Got a connection from %s" % str(addrC))
+    print fileA
     
-    currentTime = time.ctime(time.time()) + "\r\n"
-    clientSocketA.send(currentTime.encode('ascii'))
-    clientSocketB.send(currentTime.encode('ascii'))
-    clientSocketC.send(currentTime.encode('ascii'))
+    clientSocketA.send(fileA.encode('ascii'))
+    clientSocketB.send(fileB.encode('ascii'))
+    clientSocketC.send(fileC.encode('ascii'))
     
     serverSocketA.close()
     serverSocketB.close()
     serverSocketC.close()
+    
+    sys.exit()
